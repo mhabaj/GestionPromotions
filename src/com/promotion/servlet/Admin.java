e651912931bb5a19c21c0e0081dee07346cb879b
Rondeau Juliette <juliette.rondeau7@gmail.com>
2021-05-10 09:38:06 +0200

    commentaires package servlet + retest fonctionnels

--- src/com/promotion/servlet/Admin.java
+++ src/com/promotion/servlet/Admin.java
@@ -10,13 +10,13 @@ import javax.servlet.http.HttpServletResponse;
 import com.promotion.dao.DaoGenerator;
 
 /**
- * Servlet implementation class Admin
+ * Implementation de la classe admin
  */
 @WebServlet("/admin")
 public class Admin extends HttpServlet {
        
     /**
-	 * 
+	 * On génère un identifiant unique pour cette servlet
 	 */
 	private static final long serialVersionUID = 1002918475188212581L;
     
@@ -25,20 +25,29 @@ public class Admin extends HttpServlet {
 		DaoGenerator.init(this.getServletContext());
 	}
     
+    /**
+     * Constructeur par defaut de la classe Admin
+     */
     public Admin() {
     	super();
     	
     }
 
 	/**
+	 * Redefinition de la methode doGet
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @throws ServletException : Dès qu'une exception est rencontrée.
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//on effectue une redirection de la requete vers la jsp admin
 		this.getServletContext().getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
 	}
 
 	/**
+	 * Redéfinition de la methode doPost
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @throws ServletException : Dès qu'une exception est rencontrée.
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
